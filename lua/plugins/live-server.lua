dedf35de9d7241428bc95fe54b37ea14088d67a4
rvbug <rakesh.venkat@gmail.com>
2026-03-14 18:41:25 +0530

    archive

--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -3,14 +3,15 @@ return {
 
   -- for html css javascript
 
-  "barrett-ruth/live-server.nvim",
-	config = function()
-      require("live-server").setup({
-          build = 'pnpm add -g live-server',
-          cmd = { 'LiveServerStart', 'LiveServerStop' },
-          config = true
-       })
-		vim.keymap.set("n", "<leader>ss", "<cmd>LiveServerStart<cr>")
-		vim.keymap.set("n", "<leader>st", "<cmd>LiveServerStop<cr>")
-	end,
+ --  "barrett-ruth/live-server.nvim",
+	-- config = function()
+ --      require("live-server").setup({
+ --          build = 'pnpm add -g live-server',
+ --          cmd = { 'LiveServerStart', 'LiveServerStop' },
+ --          config = true
+ --       })
+	-- 	vim.keymap.set("n", "<leader>ss", "<cmd>LiveServerStart<cr>")
+	-- 	vim.keymap.set("n", "<leader>st", "<cmd>LiveServerStop<cr>")
+	-- end,
+
 }
